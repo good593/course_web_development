39adbfc6b0cbd4d0687cd8477b74b2f108df0d4d
good593 <goodwon593@gmail.com>
2024-05-09 17:42:10 +0900

    spring filter

--- 4. backend/SpringBoot/2-1. springFilterInterceptorGradle/springbasic/src/main/java/com/example/springbasic/config/filter/BasicFilter.java
+++ 4. backend/SpringBoot/2-1. springFilterInterceptorGradle/springbasic/src/main/java/com/example/springbasic/config/filter/BasicFilter.java
@@ -1,14 +1,19 @@
 package com.example.springbasic.config.filter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,12 +25,22 @@ public class BasicFilter implements Filter {
             throws IOException, ServletException {
         // TODO Auto-generated method stub
         log.info("[BasicFilter][doFilter] Start");
-        /*
-         * filter 로직 작성 
-         */
+        
+        // 전처리 
+        ContentCachingRequestWrapper httpRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper httpResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
 
         // 로직이 완료되면, 다음 filter 실행
-        chain.doFilter(request, response);
+        chain.doFilter(httpRequest, httpResponse);
+
+        // 후처리 
+        String uri = httpRequest.getRequestURI();
+        String requestContent = new String(httpRequest.getContentAsByteArray(), StandardCharsets.UTF_8);
+        String responseContent = new String(httpResponse.getContentAsByteArray(), StandardCharsets.UTF_8);
+
+        log.info("uri: "+uri);
+        log.info("requestContent: "+requestContent);
+        log.info("responseContent: "+responseContent);
     }
     
 }
